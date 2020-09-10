daf56b73b016510528c2cf4c101ac327475fa30f
Pálína Kröyer <palinakroyer@Palinas-MacBook-Pro-2.local>
2020-09-10 14:42:30 +0000

    laga

--- Reiknirit-S1/src/com/company/Percolation.java
+++ Reiknirit-S1/src/com/company/Percolation.java
@@ -1,7 +1,9 @@
 package com.company;
 
-import java.util.Arrays;
+import org.omg.Messaging.SYNC_WITH_TRANSPORT;
 
+import java.util.Arrays;
+import java.lang.IndexOutOfBoundsException;
 public class Percolation {
     int size;
     int NOpen = 0;
@@ -9,6 +11,9 @@ public class Percolation {
     int[] flowGrid;
 
     public Percolation(int N) {
+        if (N<=0) {
+            throw new java.lang.IllegalArgumentException();
+        }
         size = N;
         grid = new int[N][N];                        //Blocked = 0, Open = 1, Flow = 2
         flowGrid = new int[size * size];             // pointer array that point to the smallest connected index
@@ -21,13 +26,13 @@ public class Percolation {
         // prints out grid an flowGrid
 
         // if the group index is smaller than size then it is connected to the top
-        for (int i = 0; i < size; i++) {
+        /*for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
                 if (flowGrid[i * size + j] < size) {
                     grid[i][j] = 2;                     // updating grid
                 }
             }
-        }
+        }*/
 
         // Printing Grid out
         System.out.println("Grid:");
@@ -52,9 +57,9 @@ public class Percolation {
     // Opens the site at row,col
         if (row > size-1 || col > size-1 || col < 0 || row < 0) {
             throw new java.lang.IndexOutOfBoundsException();
+            //System.out.println("HELL");
         }
         else if (grid[row][col] == 0) {   // if blocked
-            grid[row][col] = 1;
             int number;
             int min = row * size + col;          // minimum index
             flowGrid[row * size + col] = min;    // points at itself in the beginning
@@ -69,7 +74,7 @@ public class Percolation {
                     min = number;
                 } else {
                     flowGrid[number] = min;   // otherwise then the lowest index at current union takes points at the min
-                    checkAll(number, min);     // change every pointer in the union to min
+                    //checkAll(number, min);     // change every pointer in the union to min
                 }
             }
 
@@ -82,7 +87,7 @@ public class Percolation {
                     min = number;
                 } else {
                     flowGrid[number] = min;
-                    checkAll(number, min);
+                    //checkAll(number, min);
                 }
             }
 
@@ -93,7 +98,7 @@ public class Percolation {
                     min = number;
                 } else {
                     flowGrid[number] = min;
-                    checkAll(number, min);
+                    //checkAll(number, min);
                 }
             }
 
@@ -104,12 +109,20 @@ public class Percolation {
                     min = number;
                 } else {
                     flowGrid[number] = min;
-                    checkAll(number, min);
+                    //checkAll(number, min);
                 }
             }
             NOpen += 1;  // update number of open sites
 
+
             flowGrid[row * size + col] = min;  // update new open site to its union lowest index
+            checkAll();
+            if (min<size) {
+                grid[row][col]=2;
+            }
+            else {
+                grid[row][col]=1;
+            }
         }
         else {
             System.out.println("This site is already open");
@@ -125,13 +138,26 @@ public class Percolation {
         return isOpen(target_row, target_col) || isFull(target_row, target_col);
     }
 
-    public void checkAll(int prevNum, int num) {
+    public void checkAll() {
     // Updates the whole union from previous index to current index
+        //boolean flow=num<size && prevNum>size;
+        /*System.out.println(size);
+        System.out.println(num);
+        System.out.println(prevNum);
+        System.out.println(flowGrid[6]);*/
         for (int i = 0; i < size * size; i++) {
-            if (flowGrid[i] == prevNum) {
-                flowGrid[i] = num;
+            if (flowGrid[i]!=25) {
+                if ((flowGrid[i] != flowGrid[flowGrid[i]]) || i != flowGrid[i]) {
+                    flowGrid[i] = flowGrid[flowGrid[i]];
+                    if (flowGrid[i] < size) {
+                        System.out.println(i / size + " - " + i % size);
+
+                        grid[i / size][i % size] = 2;
+                    }
+                }
             }
         }
+
     }
 
     public boolean isOpen(int row, int col) {
@@ -173,14 +199,32 @@ public class Percolation {
         int N = 5;
         Percolation P = new Percolation(N);
         P.print_grid();
-        P.open(1, 2);
+        /*P.open(1, 2);
         P.open(3, 4);
         P.open(4, 2);
         P.open(0, 1);
         P.open(1, 1);
         P.open(2, 1);
         P.open(3, 1);
+        P.open(3, 2);*/
+
+        P.open(0, 0);
+        P.open(1, 1);
         P.open(3, 2);
+        P.open(2, 2);
+        P.open(1, 2);
+        P.open(0, 2);
+
+        P.open(4, 3);
+        P.open(3, 4);
+        P.open(1, 3);
+
+        P.open(3, 3);
+/*
+        P.open(4, 0);*/
+
+
+
 
         P.print_grid();
 
