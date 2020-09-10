22acdacd624719c3b176af7cf2730ba262d4388f
jonasmark97 <59262609+jonasmark97@users.noreply.github.com>
2020-09-10 11:05:26 +0000

    Add files via upload

---
+++ Reiknirit-S1/src/com/company/Percolation.java
@@ -0,0 +1,191 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Percolation {
+    int size;
+    int NOpen = 0;
+    int[][] grid;
+    int[] flowGrid;
+
+    public Percolation(int N) {
+        size = N;
+        grid = new int[N][N];                        //Blocked = 0, Open = 1, Flow = 2
+        flowGrid = new int[size * size];             // pointer array that point to the smallest connected index
+        for (int i = 0; i < (size * size); i++) {
+            flowGrid[i] = size * size;               // all start by pointing at size*size i.e. bigger than any index
+        }
+    }
+
+    public void print_grid() {
+        // prints out grid an flowGrid
+
+        // if the group index is smaller than size then it is connected to the top
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (flowGrid[i * size + j] < size) {
+                    grid[i][j] = 2;                     // updating grid
+                }
+            }
+        }
+
+        // Printing Grid out
+        System.out.println("Grid:");
+        for (int i = 0; i < size; i++) {
+            System.out.println(Arrays.toString(grid[i]));
+        }
+
+        // printing flowGrid
+        System.out.println("\nflowGrid:");
+        for (int j = 0; j < size; j++) {
+            for (int i = 0; i < size; i++) {
+                System.out.print(flowGrid[j * size + i]+ " ");
+            }
+            System.out.print("\n");
+        }
+        System.out.print("\n");
+
+
+    }
+
+    public void open(int row, int col) {
+    // Opens the site at row,col
+        if (row > size-1 || col > size-1 || col < 0 || row < 0) {
+            throw new java.lang.IndexOutOfBoundsException();
+        }
+        else if (grid[row][col] == 0) {   // if blocked
+            grid[row][col] = 1;
+            int number;
+            int min = row * size + col;          // minimum index
+            flowGrid[row * size + col] = min;    // points at itself in the beginning
+            boolean fullOrOpen;
+
+
+            fullOrOpen = withinBounds(row - 1, col);
+            if (fullOrOpen) {
+                number = flowGrid[(row - 1) * size + col];   // takes the pointer at the location
+
+                if (min > number) {    // if pointer is lower than at it self
+                    min = number;
+                } else {
+                    flowGrid[number] = min;   // otherwise then the lowest index at current union takes points at the min
+                    checkAll(number, min);     // change every pointer in the union to min
+                }
+            }
+
+
+            fullOrOpen = withinBounds(row, col + 1);
+            if (fullOrOpen) {
+                number = flowGrid[row * size + (col + 1)];
+
+                if (min > number) {
+                    min = number;
+                } else {
+                    flowGrid[number] = min;
+                    checkAll(number, min);
+                }
+            }
+
+            fullOrOpen = withinBounds(row, col - 1);
+            if (fullOrOpen) {
+                number = flowGrid[row * size + col - 1];
+                if (min > number) {
+                    min = number;
+                } else {
+                    flowGrid[number] = min;
+                    checkAll(number, min);
+                }
+            }
+
+            fullOrOpen = withinBounds(row + 1, col);
+            if (fullOrOpen) {
+                number = flowGrid[(row + 1) * size + col];
+                if (min > number) {
+                    min = number;
+                } else {
+                    flowGrid[number] = min;
+                    checkAll(number, min);
+                }
+            }
+            NOpen += 1;  // update number of open sites
+
+            flowGrid[row * size + col] = min;  // update new open site to its union lowest index
+        }
+        else {
+            System.out.println("This site is already open");
+        }
+    }
+
+    public boolean withinBounds(int target_row, int target_col) {
+    // Returns True if (target_row, target_col) is within grid boundaries and is either open or full, otherwise false
+
+        if (target_row < 0 || target_row > size - 1 || target_col < 0 || target_col > size - 1) {
+            return false;
+        }
+        return isOpen(target_row, target_col) || isFull(target_row, target_col);
+    }
+
+    public void checkAll(int prevNum, int num) {
+    // Updates the whole union from previous index to current index
+        for (int i = 0; i < size * size; i++) {
+            if (flowGrid[i] == prevNum) {
+                flowGrid[i] = num;
+            }
+        }
+    }
+
+    public boolean isOpen(int row, int col) {
+    // Returns true if site at (row,col) is within boundaries and is open
+        if (row > size-1 || col > size-1 || col < 0 || row < 0) {
+            throw new java.lang.IndexOutOfBoundsException();
+        } else if (grid[row][col] == 1) {
+            return true;
+        } else return false;
+    }
+
+
+    public boolean isFull(int row, int col) {
+    // Returns true if site at (row,col) is within boundaries and is full
+        if (row > size-1 || col > size-1 || col < 0 || row < 0) {
+            throw new java.lang.IndexOutOfBoundsException();
+        } else if (grid[row][col] == 2) {
+            return true;
+        } else return false;
+    }
+
+    public int numberOfOpenSites() {
+    // Returns the number of open/full sites
+        return NOpen;
+    }
+
+    public boolean percolates() {
+    // Return true if the system percolates, otherwise false
+        for (int i = 0; i < size; i++) {
+            if (flowGrid[size * (size - 1) + i] < size) {   // if any of the bottom row points at any of the index in the top row
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        int N = 5;
+        Percolation P = new Percolation(N);
+        P.print_grid();
+        P.open(1, 2);
+        P.open(3, 4);
+        P.open(4, 2);
+        P.open(0, 1);
+        P.open(1, 1);
+        P.open(2, 1);
+        P.open(3, 1);
+        P.open(3, 2);
+
+        P.print_grid();
+
+        System.out.println("Percolates?: " + P.percolates());
+
+
+    }
+}
